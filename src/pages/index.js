4b87ea047db501a26c3500ab65470273ef7aef18
fer g <arielgeronimo517@gmail.com>
2021-02-06 23:18:56 -0600

    agregando componente de informacion en la pagina principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
+import Information from "../components/Information";
 
 const IndexPage = () => (
   <Layout>
+    <Information />
   </Layout>
 )
 
