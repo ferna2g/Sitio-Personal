78d639b5f2a779178701cff546074dcfacfa0f0a
fer g <arielgeronimo517@gmail.com>
2021-02-06 21:21:46 -0600

    configurando el proyecto

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,8 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-transformer-json`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
