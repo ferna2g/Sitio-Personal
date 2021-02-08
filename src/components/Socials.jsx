005baa1bbafe23b37ac77834cedff5a699e3dc17
fer g <arielgeronimo517@gmail.com>
2021-02-07 18:48:01 -0600

    creando componente para redes sociales

---
+++ src/components/Socials.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import IconGithub from '../imgs/icons/github.svg';
+import IconLinkedin from '../imgs/icons/linkedin.svg';
+import IconTwitter from '../imgs/icons/twitter.svg';
+
+
+const mediaSocials = [
+  {
+    icon:<IconLinkedin />,
+    link:"https://www.linkedin.com/in/ariel-fernando-gerónimo-gil-40292517b/",
+  },
+  {
+    icon:<IconGithub />,
+    link: "https://www.github.com/ferna2g",
+  },
+  {
+    icon:<IconTwitter/>,
+    link:"https://www.twitter.com/ferna_gl",
+  }
+]
+
+
+const Socials = (props) => {
+
+  return (
+    <div className="fotos mt-10">
+      <svg className="fill-current">
+      {mediaSocials.map((media, index) => (
+        <a
+          key={index}
+          href={media.link}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          {media.icon}
+        </a>
+      ))}
+      </svg>
+    </div>
+  )
+}
+
+export default Socials;
