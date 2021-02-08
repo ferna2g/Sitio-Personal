e59c8a009f91415f19cf2ef550c0ea4d817b4318
fer g <arielgeronimo517@gmail.com>
2021-02-07 18:44:41 -0600

    consulta dinamica

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,28 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({graphql,actions})=>{
+
+  const result = await graphql(`
+      {
+        allEducationJson {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+      }
+    `);
+
+    result.data.allEducationJson.edges.forEach(element=>{
+      const { node } = element;
+      actions.createPage({
+        path: node.slug,
+        component: path.resolve('./src/templates/education.jsx'),
+        context:{
+          slug: node.slug
+        }
+      });
+    });
+
+}
