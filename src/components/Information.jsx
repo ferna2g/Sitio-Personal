f1980093ce17162b255d92cf75d734bb1bc616d9
fer g <arielgeronimo517@gmail.com>
2021-02-07 18:47:06 -0600

    agregando componente de redes sociales

---
+++ src/components/Information.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Socials from "./Socials";
+import ilustration from '../imgs/5f7a57894e7f1.jpg';
+
+const Information = () => {
+
+  return (
+    <div className="bg-white mx-auto p-8">
+      <div className="flex-1">
+        <div className="text-center">
+          <h2 className="font-bold text-black-100 p-3 text-4xl mx-auto">Ariel Fernando Gerónimo Gil</h2>
+        </div>
+        <div className="md:flex bg-white rounded-lg p-8 justify-center">
+            <img className="h-56 w-56 md:h-64 md:w-64 rounded-full mx-auto md:mx-0 md:mr-12" src={ilustration} alt="fotos"/>
+            <div className="text-center md:text-left">
+              <h2 className="text-lg text-2xl mt-4">Programador Junior</h2>
+              <h2 className="text-lg mt-4">Autodidacta, entusiasta</h2>
+              <h2 className="font-bold text-lg mt-4 md:ml-10">Conoce mas</h2>
+              <svg className="animate-bounce w-6 h-6 ...">
+
+              </svg>
+            </div>
+        </div>
+      </div>
+      <Socials />
+    </div>
+  )
+}
+
+export default Information;
