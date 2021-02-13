b96e2f46c9057f12e207d467d4fe780b636095ff
fer g <arielgeronimo517@gmail.com>
2021-02-12 21:21:21 -0600

    modificando estilos para ser responsive

--- src/templates/education.jsx
+++ src/templates/education.jsx
@@ -7,7 +7,7 @@ const Education = (props) => {
     const pageData = props.data.educationJson;
 
     return (
-      <div>
+      <div className="bg-gray-300">
       <Nav />
         <header className="py-12">
           <div className="max-w-4xl mx-auto">
@@ -15,30 +15,24 @@ const Education = (props) => {
             <p className="text-xl text-center">{pageData.description}</p>
           </div>
         </header>
-        <ul className="max-w-4xl mx-auto pb-8">
+        <ul className="p-10 grid grid-cols-1 md:grid-cols-3 gap-5">
         {
           pageData.items.map((item,index)=>(
-            <li className="bg-yellow-200 font-bold shadow mt-4 flex" key={index}>
-              <p className="vertical-tex"></p>
-              <div className="flex items-center flex-1 p-8">
-                <div className="flex-1">
-                <h3>{item.name}</h3>
+            <li className="font-bold shadow-lg mx-4 my-4 rounded" key={index}>
+              <div className=" p-8">
+                <div className="text-center py-2 mx-auto ">
+                    <h3>{item.name}</h3>
+                </div>
                 {
                   //si el item tiene un degree muestra el siguiente span
-                  item.degree && <span className="inline-block py-2 px-4 radius bg-orange-400 text-white font-bold">{item.degree}</span>
+                  item.degree && <div className="py-2 px-4 text-gray-800 text-center font-bold">{item.degree}</div>
                 }
                 {
                   //si tiene url muestre la siguiente etiqueta a
-                  item.url && <a href={item.url} target="_blank" rel="noopener noreferrer" className="btn mt-4 inline-block">Ir al Curso</a>
+                  item.url && <div className="px-6 py-4 text-center">
+                    <a href={item.url} target="_blank" rel="noopener noreferrer" className="bg-gray-200 rounded px-3 py-1 text-sm text-center font-semibold text-gray-70 mx-auto">Ir al Curso</a>
+                  </div>
                 }
-                {
-                  //si el item tiene un degree muestra el siguiente span
-                  item.platform && <span className="inline-block p-2 border-radius bg-orange-400 text-white">{item.platform}</span>
-                }
-                </div>
-                <div className="inline-block">
-                  <span className="inline-block p-2 text-2xl bg-yellow-200 text-black-700">{item.score}</span>
-                </div>
               </div>
             </li>
           ))
@@ -59,7 +53,6 @@ export const query = graphql`
         items{
           name
           degree
-          score
           url
         }
       }
