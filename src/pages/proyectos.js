ea16867d62da1a7a667b80c655744ec5a80d2939
fer g <arielgeronimo517@gmail.com>
2021-02-12 21:26:46 -0600

    desplegando el componente de proyectos en la pagina de proyectos

---
+++ src/pages/proyectos.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout";
+import Projects from "../components/projects";
+
+const Proyectos = () => (
+  <Layout>
+    <Projects />
+  </Layout>
+)
+
+export default Proyectos
