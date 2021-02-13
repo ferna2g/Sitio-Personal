544a88c8063b280e151149d62f1b232fef2c14cb
fer g <arielgeronimo517@gmail.com>
2021-02-12 21:16:43 -0600

    agregando pagina de proyectos

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,12 @@ import { Link } from 'gatsby';
 
 const navItems = [
   {
-    caption: 'Inicio',
-    href: '/'
+    caption: 'Proyectos',
+    href: '/proyectos'
+  },
+  {
+    caption: 'Skills',
+    href: '/skills'
   },
   {
     caption: 'Formación Académica',
@@ -17,11 +21,7 @@ const navItems = [
   {
     caption: 'Otros Logros Académicos',
     href: '/others'
-  },
-  {
-    caption: 'Skills',
-    href: '/skills'
-  }
+  }  
 ]
 
 const Nav = (props) => {
@@ -34,7 +34,7 @@ const Nav = (props) => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-blue-600 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">Fer/DevJr</span>
+        <Link to="/" className="font-semibold text-xl tracking-tight">Fer/DevJr</Link>
       </div>
       <div className="block md:hidden">
         <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
@@ -42,14 +42,13 @@ const Nav = (props) => {
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" ><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
       </div>
-      <ul id="menu" className="w-full block flex-grow md:flex md:items-center md:w-auto hidden">
+      <ul id="menu" className="w-full block md:flex md:items-center md:w-auto hidden">
         {
           navItems.map((item, index) => (
-            <li className="text-sm md:flex-grow" key={index}>
-              <Link className="hover:bg-blue-400 block mt-4 md:inline-block md:mt-0 text-white hover:text-white mr-4" to={item.href}>{item.caption}</Link>
+            <li className="text-lg md:flex-grow" key={index}>
+              <Link className="hover:bg-blue-500 block mt-4 md:inline-block md:mt-0 text-white hover:text-white mr-4" to={item.href}>{item.caption}</Link>
             </li>
           ))
-
         }
       </ul>
     </nav>
