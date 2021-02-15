43de82e13650b072b5c122ac88421deef30b6983
fer g <arielgeronimo517@gmail.com>
2021-02-14 21:32:48 -0600

    agregando helmet

--- src/pages/proyectos.js
+++ src/pages/proyectos.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout";
 import Projects from "../components/projects";
+import { Helmet } from "react-helmet";
 
 const Proyectos = () => (
   <Layout>
+    <Helmet title="Proyectos"></Helmet>
     <Projects />
   </Layout>
 )
