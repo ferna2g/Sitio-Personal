c796cc7206e96c6eed4f4fd420f76dac866776b8
fer g <arielgeronimo517@gmail.com>
2021-02-14 21:31:15 -0600

    agregando helmet para titulo de pagina

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../components/layout";
 import Information from "../components/Information";
+import { Helmet } from "react-helmet";
 
 const IndexPage = () => (
   <Layout>
+    <Helmet title="Home"></Helmet>
     <Information />
   </Layout>
 )
