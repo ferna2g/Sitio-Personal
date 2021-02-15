f4c8decd15dfa68b70f6ab144b7c9c5a273d0ad3
fer g <arielgeronimo517@gmail.com>
2021-02-14 21:30:27 -0600

    cambiando colores de bg

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -6,12 +6,12 @@ import ilustration from '../imgs/5f7a57894e7f1.jpg';
 const Information = () => {
 
   return (
-    <div className="bg-white mx-auto p-8">
+    <div className="bg-gray-200 mx-auto p-8">
       <div className="flex-1">
         <div className="text-center">
           <h2 className="font-bold text-black-100 p-3 text-4xl mx-auto">Ariel Fernando Gerónimo Gil</h2>
         </div>
-        <div className="md:flex bg-white rounded-lg p-8 justify-center">
+        <div className="bg-gray-200 md:flex bg-white rounded-lg p-8 justify-center">
             <img className="h-56 w-56 md:h-64 md:w-64 rounded-full mx-auto md:mx-0 md:mr-12" src={ilustration} alt="fotos"/>
             <div className="text-center md:text-left">
               <h2 className="text-lg text-2xl mt-4">Programador Junior</h2>
